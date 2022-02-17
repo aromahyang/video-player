528e2adf929e51e49b1fce838aade30039d90bc2
aromahyang <haeng1605@gmail.com>
2022-02-17 23:52:29 +0900

    :art: ref prop 제거

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -52,6 +52,22 @@ function Video() {
     setPlaying(false);
   };
 
+  const handleSkipClick = () => {
+    if (videoRef.current) {
+      let target = 0;
+      if (current <= videoInfo.opening[1]) {
+        target = videoInfo.opening[1] + 1;
+      } else if (current >= videoInfo.ending[0]) {
+        target = videoInfo.ending[1] + 1;
+      }
+      setCurrent(target);
+      videoRef.current.currentTime = target;
+      if (!playing) {
+        setPlaying(true);
+      }
+    }
+  };
+
   return (
     <div ref={videoContainerRef} css={styles.container}>
       <video
@@ -73,11 +89,10 @@ function Video() {
         setCurrent={setCurrent}
       />
       <Skip
-        video={videoRef.current}
         opening={videoInfo.opening ?? []}
         ending={videoInfo.ending ?? []}
         current={current}
-        setCurrent={setCurrent}
+        onClick={handleSkipClick}
       />
     </div>
   );
